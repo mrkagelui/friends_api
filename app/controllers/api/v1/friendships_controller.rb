911983d0a6cae8ee8cecd56c753a381797d3301d
mrkagelui <jasonlv@live.com>
2018-04-18 20:58:55 +0800

    implement get_common_friends

--- app/controllers/api/v1/friendships_controller.rb
+++ app/controllers/api/v1/friendships_controller.rb
@@ -16,4 +16,13 @@ class Api::V1::FriendshipsController < ApplicationController
     friends = user.friends.map { |friend_id| User.find(friend_id).email }
     render json: { success: true, friends: friends, count: friends.length }, status: :ok
   end
+
+  def get_common_friends
+    user_emails = params[:friends]
+    common_friend_ids = Array.new
+    user_emails.each { |user_email| common_friend_ids = (0 == common_friend_ids.length) ? 
+      User.find_by!(email: user_email).friends : common_friend_ids & User.find_by!(email: user_email).friends }
+    common_friend_emails = common_friend_ids.map { |e| User.find(e).email }
+    render json: { success: true, friends: common_friend_emails, count: common_friend_emails.length }, status: :ok
+  end
 end
