cf87d16d4c37da01bd87b7106b6d33dea64a4232
mrkagelui <jasonlv@live.com>
2018-04-18 23:33:39 +0800

    modify route to accommodate heroku

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
-  namespace :api, defaults: {format: :json},
-  			constraints: {subdomain: 'api'}, path: '/' do
+  namespace :api, defaults: {format: :json}, path: '/api' do
     scope module: :v1, constraints: APIConstraints.new(version: 1, default: true) do
       post 'signup', to: 'users#create'
       post 'add_friend', to: 'friendships#create'
