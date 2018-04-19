b7c785d4438985fae8b3ca7edce80f0533a697ea
mrkagelui <jasonlv@live.com>
2018-04-19 14:54:37 +0800

    Fix bug where mentioned email is invalid

--- app/controllers/api/v1/update/receivers_controller.rb
+++ app/controllers/api/v1/update/receivers_controller.rb
@@ -5,7 +5,7 @@ class Api::V1::Update::ReceiversController < ApplicationController
     followers = sender.followers
     mentioned_emails = update_params[:text].scan(
     	/\b([A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,6})\b/).flatten
-    mentioneds = mentioned_emails.map { |e| User.find_by(email: e) }
+    mentioneds = mentioned_emails.map { |e| User.find_by(email: e) } .compact
     blockers = sender.blockers
     receivers = (friends | followers | mentioneds) - blockers
     receiver_emails = receivers.map { |e| e.email }
